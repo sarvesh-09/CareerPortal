7882a3f701100adcde4a986b88a6acb716aa7da0
sarvesh-09 <sarveshpatil169@gmail.com>
2025-08-29 14:48:10 +0530

    user and company repository added

---
+++ src/main/java/com/example/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+    Optional<User> findByEmail(String email);
+    User findByEmailAndPassword(String email, String password);
+   // Optional<User> findById(int id);
+    
+}
